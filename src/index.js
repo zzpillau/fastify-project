c0b528c15e8d8136385b36f3ff53e99915c1f719
zzpillau <zzpillau@gmail.com>
2025-07-15 20:12:07 +0200

    init: new project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import fastify from 'fastify'
+
+const app = fastify()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen({ port }, () => {
+  console.log(`Example app listening on port ${port}`)
+})
