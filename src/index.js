f7ab22c205357210e40194d957469a1c79ccee93
zzpillau <zzpillau@gmail.com>
2025-07-16 17:10:52 +0200

    feat: lesson 5 - HTTP Session

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import fastify from 'fastify'
 
-const app = fastify()
+const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname'
+      }
+    }
+  }
+})
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hi, Fastify!')
+  res.send('I\'m Fastify project!')
+})
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name
+  res.send(name ? `Hello, ${name}!` : `Hello, World!`)
 })
 
 app.get('/users', (req, res) => {
-  res.send('GET /users')
+  res.header('Content-Type', 'text/html; charset=utf-8')
+  res.log.info({msg: 'Some info about the current request'})
+  res.send(`
+    <html>
+      <head><title>Users</title></head>
+      <body>
+        <h1>Список пользователей</h1>
+        <p>Тут будет HTML, а не просто текст.</p>
+      </body>
+    </html>
+  `)
 })
 
 app.post('/users', (req, res) => {
